03d616a2f6404b3e732eb2841625ce97e9409dce
junesaruta <152201966+junesaruta@users.noreply.github.com>
2023-11-29 19:27:50 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import datastr.ArrayStack;
+
+public class Main {
+    static ArrayStack arrayStack=new ArrayStack(1);
+    public static void main(String[] args) {
+        String str="53+2*1-";//profix
+        String operation="+-*/";
+        if(str.split("") instanceof){
+
+        }
+    }
+}
